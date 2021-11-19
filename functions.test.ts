6b56cca801f9a087372b8fcc521a3f4d45f69737
Jessica Steen <jessicalsteen@yahoo.com>
2021-11-19 11:16:51 -0700

    function test.ts

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,18 @@
+// check that shuffleArray returns an array
+// check that it returns an array of the same length as the argument sent in
+// check that all the same items are in the array
+// check that the items have been shuffled around
+
+
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('shuffleArray should return an array', () => {
+        expect(typeof shuffleArray).toBe("array") 
+    });
+
+    test('returns array of same length', () => {
+        expect(shuffleArray).toEqual(expect.arrayContaining(shuffleArray.length))
+    });
+    
 })
